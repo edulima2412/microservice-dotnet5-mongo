b8a5957819b7012b18667d3c98a669f485c9d295
Eduardo Lima <carlos.rocha@compasso.com.br>
2021-09-07 08:32:38 -0300

    configuration docker-compose

--- src/Catalog.Api/Startup.cs
+++ src/Catalog.Api/Startup.cs
@@ -30,7 +30,7 @@ namespace Catalog
       services.AddControllers();
       services.AddSwaggerGen(c =>
       {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog", Version = "v1" });
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog Api", Version = "v1" });
       });
     }
 
@@ -41,7 +41,7 @@ namespace Catalog
       {
         app.UseDeveloperExceptionPage();
         app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog v1"));
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog Api v1"));
       }
 
       app.UseHttpsRedirection();
