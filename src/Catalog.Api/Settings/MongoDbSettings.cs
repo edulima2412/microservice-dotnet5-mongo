e5fe95bb91ba93476f391cd5da6fd723da1a7451
Eduardo Lima <carlos.rocha@compasso.com.br>
2021-09-09 20:46:37 -0300

    Configurate mongo auth

--- src/Catalog.Api/Settings/MongoDbSettings.cs
+++ src/Catalog.Api/Settings/MongoDbSettings.cs
@@ -1,14 +1,17 @@
 ﻿namespace Catalog.Api.Settings
 {
-    public class MongoDbSettings
+  public class MongoDbSettings
+  {
+    public string Host { get; set; }
+    public string Port { get; set; }
+    public string User { get; set; }
+    public string Password { get; set; }
+    public string ConnectionString
     {
-        public string Host { get; set; }
-        public string Port { get; set; }
-        public string ConnectionString {
-            get
-            {
-                return $"mongodb://{Host}:{Port}";
-            }
-        }
+      get
+      {
+        return $"mongodb://{User}:{Password}@{Host}:{Port}";
+      }
     }
+  }
 }
